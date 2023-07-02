ce63771b83364032747747b75d6b508b739fea2c
Pedro <gm.pedro.fernandes@gmail.com>
2023-07-02 14:31:49 -0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Turing_machine
-Turing Machine Simulator for content creation work on theory of computation - UDESC/CCT
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import "./styles/style.scss"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./assets/videos/tape-processing.webm"
